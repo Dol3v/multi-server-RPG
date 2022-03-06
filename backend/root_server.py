e92179f738ae63b143cc0bfcd2bacbf1b7ba6a81
Dol3v <dol12dab@gmail.com>
2022-03-06 23:31:39 +0200

    added basic server

--- backend/root_server.py
+++ backend/root_server.py
@@ -1,8 +1,27 @@
-from database import *
+import socket
+
+from common.comms import PacketID
+from authentication import *
+
+
+def handle_signup_request(conn: DefaultConnection, db: SqlDatabase):
+    username, password = recv_credentials(conn)
+    result, error_msg = signup(username, password, db)
+    if result:
+        conn.send_status(PacketID.SERVER_OK)
+    else:
+        conn.send(error_msg.encode(), PacketID.SERVER_NOK)
+
 
 if __name__ == "__main__":
-    host = input("[Enter host]: ")
-    db = Database(host)
-    db.write_tables()
+    with SqlDatabase("127.0.0.1", "dummyPass") as database, socket.socket() as sock:
+        database.write_tables()
+        sock.bind(("127.0.0.1", 5000))
+        sock.listen()
+        while True:
+            client_conn, addr = sock.accept()
+            with DefaultConnection(client_conn) as connection:
+                handle_signup_request(connection, database)
+
 
     
