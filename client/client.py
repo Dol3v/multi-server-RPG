b6f8893067b6e779c68a39dec0467e9ab1cc7be8
Dol3v <dol12dab@gmail.com>
2022-03-06 23:30:47 +0200

    updated client to sign in

--- client/client.py
+++ client/client.py
@@ -1,34 +1,42 @@
-# to import from a dir
+import socket
 import sys
-sys.path.append( '.' )
+
+from base64 import urlsafe_b64encode
+from cryptography.fernet import Fernet
 
 from common.comms import DefaultConnection, PacketID
-from cryptography.fernet import Fernet
 from game import Game
 
+# to import from a dir
+sys.path.append('.')
 
 
-def login(username: str, password: str, conn: DefaultConnection):
+def send_credentials(username: str, password: str, conn: DefaultConnection):
     """
     Use: login to the server
     """
-    fernet = Fernet(conn.key)
+    fernet = Fernet(urlsafe_b64encode(conn.key))
 
     username_token = fernet.encrypt(username.encode())
     password_token = fernet.encrypt(password.encode())
 
     conn.send(username_token, PacketID.INITIAL_AUTH)
     conn.send(password_token, PacketID.INITIAL_AUTH)
-    
+
 
 if __name__ == "__main__":
+    with socket.socket() as sock:
+        sock.connect(("127.0.0.1", 5000))
+        with DefaultConnection(sock) as connection:
+            user = input("Enter username: ")
+            user_pass = input("Enter password: ")
 
-    with DefaultConnection() as conn:
-        username = input("Enter username: ")
-        password = input("Enter password: ")
+            send_credentials(user, user_pass, connection)
 
-        login(username, password, conn)
-
-
-        game = Game()
-        game.run()
+            response = connection.recv()
+            if response.packet_type == PacketID.SERVER_NOK:
+                print(response.content)
+                sys.exit(1)
+            print("Success!")
+            game = Game()
+            game.run()
