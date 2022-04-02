7f1305acbc69a2807d78a10f164825af006fbc56
programmer1024r <reempro112@gmail.com>
2022-04-02 13:11:42 +0300

    added redirect message

--- client/connect_screen.py
+++ client/connect_screen.py
@@ -152,8 +152,10 @@ class ConnectScreen:
                 return
             self.shared_key = do_ecdh(conn)
             send_credentials(username, password, conn, self.shared_key, is_login)
-            success, error_message = get_login_response(conn)
+            ip, success, error_message = get_login_response(conn)
+            print(f"{ip=}")
             self.game_server_addr = (ip, SERVER_PORT)
+
             if not success:
                 print(error_message)
                 self.sock = None
