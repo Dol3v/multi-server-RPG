46f74616b9befd44fb020d9234907fa63e9d0510
Dol3v <dol12dab@gmail.com>
2022-04-04 17:00:08 +0300

    fixed some graphics bugs

--- client/consts.py
+++ client/consts.py
@@ -44,7 +44,7 @@ ENTITY_DATA = {
     ARROW_TYPE: ("entity/arrow/projectile.png", [], 10, 1),
     MOB_TYPE: ("entity/dino/lizard.png", ["entity/dino/lizard.png", "entity/dino/lizard_run_0.png",
                                           "entity/dino/lizard_run_1.png",
-                                          "entity/dino/lizard_run_2.png", "entity/dino/lizard_run_3.png"], 1, 3),
+                                          "entity/dino/lizard_run_2.png", "entity/dino/lizard_run_3.png"], 1, 4),
     PLAYER_TYPE: ("character/knight/knight.png", ["character/knight/move_0.png",
                                                   "character/knight/move_1.png", "character/knight/move_2.png"],
                   10, PLAYER_SIZE_MULTIPLIER)
