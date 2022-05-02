a9e2bd8677386937ba03c747a55969f13ba50e9b
Dol3v <dol12dab@gmail.com>
2022-05-02 16:50:19 +0300

    refactored server_controlled_entities.py

--- backend/logic/server_controlled_entities.py
+++ backend/logic/server_controlled_entities.py
@@ -3,8 +3,7 @@ import sched
 import time
 
 from backend.backend_consts import FRAME_TIME
-from backend.logic.entity_logic import EntityManager, Combatant
-from common.consts import EntityType, MIN_HEALTH, PROJECTILE_SPEED, MOB_SPEED
+from backend.logic.entity_logic import EntityManager
 
 
 def server_controlled_entities_update(entities_manager: EntityManager, s):
@@ -19,67 +18,20 @@ def update_projectiles(entities_manager: EntityManager):
     """Update projectile position, ttl and existence.
        In addition, lowers entities HP, and kill them if needed
        """
-    to_remove = []
     with entities_manager.projectile_lock:
-        for projectile in entities_manager.projectiles.values():
-            projectile.ttl -= 1
-            if projectile.ttl == 0:
-                logging.debug(f"[debug] gonna remove uuid={projectile.uuid}, ttl=0")
-                to_remove.append(projectile)
-                continue
-
-            intersection = entities_manager.get_collidables_with(projectile.pos, projectile.uuid,
-                                                                 kind=EntityType.PROJECTILE)
-            should_remove = False
-            if intersection:
-                for kind, identifier in intersection:
-                    if kind == EntityType.PROJECTILE:
-                        continue
-                    if kind == EntityType.PLAYER or kind == EntityType.MOB:
-                        combatant: Combatant = entities_manager.get(identifier, EntityType.MOB)
-                        logging.info(f"Projectile {projectile} hit {combatant}")
-                        should_remove = True
-                        combatant.health -= projectile.damage
-                        if combatant.health <= MIN_HEALTH:
-                            logging.info(f"[update] killed {combatant=}")
-                            # remove entity on
-                            entities_manager.remove_entity(combatant)
-                        logging.debug(f"Updated player {identifier} health to {combatant.health}")
-                if should_remove:
-                    to_remove.append(projectile)
-                    logging.debug(f"[debug] gonna remove uuid={projectile.uuid}, nonzero intersection")
-                    continue
-
-            entities_manager.update_entity_location(projectile,
-                                                    (
-                                                        projectile.pos[0] + int(
-                                                            PROJECTILE_SPEED * projectile.direction[0]),
-                                                        projectile.pos[1] + int(
-                                                            PROJECTILE_SPEED * projectile.direction[1])),
-                                                    EntityType.PROJECTILE)
-    for projectile in to_remove:
-        entities_manager.remove_entity(projectile)
-        logging.info(f"[update] removed projectile {projectile.uuid}")
+        to_remove = filter(lambda p: not p.advance_per_tick(entities_manager), entities_manager.projectiles.values())
+        for projectile in to_remove:
+            entities_manager.remove_entity(projectile)
+            logging.info(f"[update] removed projectile {projectile.uuid}")
 
 
 def update_mobs(entities_manager: EntityManager):
     """Update mobs position. In addition, attack if mob is locked on target"""
     with entities_manager.mob_lock:
-        for mob in entities_manager.mobs.values():
-            mob.update_direction(entities_manager)
-            colliding = entities_manager.get_collidables_with(mob.pos, mob.uuid, kind=EntityType.MOB)
-
-            for kind, identifier in colliding:
-                if kind == EntityType.PROJECTILE:
-                    continue
-                mob.direction = 0., 0.  # TODO: refactor a bit into update_mob_directions
-
-            if mob.on_player:
-                mob.item.on_click(mob, entities_manager)
-                # attack(entities_manager, mob, mob.weapon)
-            entities_manager.update_entity_location(mob, (mob.pos[0] + int(mob.direction[0] * MOB_SPEED),
-                                                          mob.pos[1] + int(mob.direction[1] * MOB_SPEED)),
-                                                    EntityType.MOB)
+        to_remove = filter(lambda m: not m.advance_per_tick(entities_manager), entities_manager.mobs.values())
+        for mob in to_remove:
+            entities_manager.remove_entity(mob)
+            logging.info(f"[update] killed mob {mob.uuid}")
 
 
 def server_entities_handler(entities_manager):
